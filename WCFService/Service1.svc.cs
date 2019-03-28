e6a301919f3b665e4df942dd6fca8ebf29edc2d1
Fernando V <fvalarinon@gmail.com>
2019-03-28 15:49:07 +0100

    initial

--- WCFService/Service1.svc.cs
+++ WCFService/Service1.svc.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 namespace WCFService
 {
+
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
@@ -26,6 +27,7 @@ namespace WCFService
             if (list.Count() > 0)
             {
                 entities.gradeInfoes.Remove(list.SingleOrDefault());
+                entities.SaveChanges();
                 return true;
             }
             return false;
